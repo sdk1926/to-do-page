989ec2ea1219e7b47b61100fe3a21258414c1449
dong gyu <sdk1926@naver.com>
2021-02-27 01:20:11 +0900

    retouch title input
    chage title input default value
    retouch title margin

--- gretting.js
+++ gretting.js
@@ -24,7 +24,7 @@ function askForName() {
 function paintGreeting(text) {
   form.classList.remove(SHOWING_CN);
   greeting.classList.add(SHOWING_CN);
-  greeting.innerText = `${text}`;
+  greeting.innerText = `${text}'S To Do List`;
 }
 
 function loadName() {
